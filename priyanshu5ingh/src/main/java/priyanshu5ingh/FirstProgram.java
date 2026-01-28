cb2831ac8569a2710cc65d85047cf704366ba62a
sachin <sachinchetty2707@gmail.com>
2026-01-28 11:58:35 +0530

    Update FirstProgram.java
    updated the distance functions

--- priyanshu5ingh/src/main/java/priyanshu5ingh/FirstProgram.java
+++ priyanshu5ingh/src/main/java/priyanshu5ingh/FirstProgram.java
@@ -3,6 +3,14 @@ package priyanshu5ingh;
 public class FirstProgram {
 
 	public static void main(String[] args) {
+		//SACHIN HAS EDITTTED
+		Scanner sc= new Scanner(System.in);
+		System.out.println(x: "Enter the number of kms");
+		float kms = sc.nextfloat();
+		float 1kms = 0.62137119F;
+		float Miles = 1kms*kms;
+		System.out.print(Miles);
+			
 		// TODO Auto-generated method stub
 
 	}
