959dd941288e6d65664e997f5c7b8e6b5b80a3af
priyanshu5ingh <priyanshusinghrajput007@gmail.com>
2026-04-08 12:33:06 +0530

    Fixed Java syntax errors

--- priyanshu5ingh/src/main/java/FirstProgram.java
+++ priyanshu5ingh/src/main/java/FirstProgram.java
@@ -1,18 +1,29 @@
 package priyanshu5ingh;
 
+import java.util.Scanner; // Required to use Scanner
+
 public class FirstProgram {
 
-	public static void main(String[] args) {
-		//SACHIN HAS EDITTTED
-		Scanner sc= new Scanner(System.in);
-		System.out.println(x: "Enter the number of kms");
-		float kms = sc.nextfloat();
-		float 1kms = 0.62137119F;
-		float Miles = 1kms*kms;
-		System.out.print(Miles);
-			
-		// TODO Auto-generated method stub
-
-	}
-System.out.println("Conflict arise");
+    public static void main(String[] args) {
+        //SACHIN HAS EDITTTED
+        Scanner sc = new Scanner(System.in);
+        
+        // Removed the "x:"
+        System.out.println("Enter the number of kms"); 
+        
+        // Capitalized the F in nextFloat
+        float kms = sc.nextFloat(); 
+        
+        // Renamed variable so it doesn't start with a number
+        float conversionRate = 0.62137119F; 
+        float miles = conversionRate * kms;
+        
+        System.out.println(miles);
+            
+        // Moved this statement INSIDE the main method
+        System.out.println("Conflict arise"); 
+        
+        // TODO Auto-generated method stub
+        sc.close(); // It's good practice to close your scanner!
+    }
 }
